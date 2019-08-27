a5ced6d068f5f949bb28e22c9eb8c725ea0fd381
applego <applegorillappa@gmail.com>
2019-08-27 17:00:02 +0900

    左右に動くように

--- js/main.js
+++ js/main.js
@@ -20,6 +20,29 @@ var array = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
+//現在動いているブロックがどこにあるかを保持する配列を追加
+var move = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
 // 描画処理
 function draw() {
@@ -39,25 +62,30 @@ function draw() {
 
 // 落下処理
 function fall() {
-    var under = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]; // 下の行にブロックがあるかどうかを保持する配列
+    var under = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
     for (var i = 19; i >= 0; i--) {
         for (var j = 0; j < 10; j++) {
             if (under[j] == 0) {
                 if (array[i][j] == 0) {
-                    // 下に何もなくそのマスがブロックでもないとき
+                    // 下に何もなくブロックでもない
                     under[j] = 0;
                 } else {
-                    // 下に何もなくそのマスがブロックであるとき
+                    // 下に何もなくブロックであるとき
                     array[i + 1][j] = array[i][j];
                     array[i][j] = 0;
+                    // moveも一緒に動かす
+                    if (move[i][j] == 1) {
+                        move[i][j] = 0;
+                        move[i + 1][j] = 1;
+                    }
                     under[j] = 0;
                 }
             } else {
                 if (array[i][j] == 0) {
-                    // 下がブロックでそのマスがブロックでないとき
+                    // 下がブロックでブロックでないとき
                     under[j] = 0;
                 } else {
-                    // 下がブロックでそのマスがブロックのとき
+                    // 下がブロックでブロックのとき
                     under[j] = 1;
                 }
             }
@@ -73,18 +101,62 @@ function genBlock(blockNum) {
             array[1][5] = blockNum;
             array[2][5] = blockNum;
             array[3][5] = blockNum;
+            // moveも一緒に変更する
+            move[0][5] = 1;
+            move[1][5] = 1;
+            move[2][5] = 1;
+            move[3][5] = 1;
             break;
     }
 }
-//スペースキーを押したとき
-document.onkeydown = function(e) { // キーボードの処理はこのように書きます
+
+function moveBlockRight() {
+    for (var i = 19; i >= 0; i--) {
+        var newMove = move[i].concat();
+        for (var j = 8; j >= 0; j--) {
+            if (move[i][j] == 1) {
+                array[i][j + 1] = array[i][j];
+                array[i][j] = 0;
+                newMove[j + 1] = 1;
+                newMove[j] = 0;
+            }
+        }
+        move[i] = newMove;
+    }
+}
+
+function moveBlockLeft() {
+    for (var i = 19; i >= 0; i--) {
+        var newMove = move[i].concat();
+        for (var j = 1; j < 10; j++) {
+            if (move[i][j] == 1) {
+                array[i][j - 1] = array[i][j];
+                array[i][j] = 0;
+                newMove[j - 1] = 1;
+                newMove[j] = 0;
+            }
+        }
+        move[i] = newMove;
+    }
+}
+
+//キーボード操作
+document.onkeydown = function(e) {
     switch (e.code) {
         case "Space":
             genBlock(1);
             break;
+        // ここから下を追加する
+        case "ArrowRight":
+            moveBlockRight();
+            break;
+        case "ArrowLeft":
+            moveBlockLeft();
+            break;
     }
     draw();
 }
+
 
 draw(); // 読込が完了したらまず表示
 setInterval(function() {
